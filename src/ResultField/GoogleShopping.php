727b973d814480028a06e5bee4301b041cc834bc
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-05-02 12:19:49 +0200

    Update GoogleShopping.php

--- src/ResultField/GoogleShopping.php
+++ src/ResultField/GoogleShopping.php
@@ -108,7 +108,7 @@ class GoogleShopping extends ResultFields
         /**
          * @var LanguageMutator $languageMutator
          */
-		$languageMutator = pluginApp(LanguageMutator::class, ['language' => [$settings->get('lang')]]);
+		$languageMutator = pluginApp(LanguageMutator::class, ['languages' => [$settings->get('lang')]]);
 
         /**
          * @var SkuMutator $skuMutator
