651ca1da1c4f7a9f9a3d200bd8b7c5b24a433dc8
Denis Betegh <denis.betegh@plentymarkets.com>
2021-01-08 10:55:55 +0200

    Added the google shopping attributes who were missing

--- src/Catalog/DataProviders/GeneralDataProvider.php
+++ src/Catalog/DataProviders/GeneralDataProvider.php
@@ -51,9 +51,23 @@ class GeneralDataProvider extends BaseDataProvider
                 'key' => 'condition',
                 'label' => 'Condition',
                 'required' => false
+            ],
+            //FIXME REQUIRED BY DEFAULT
+            [
+                'key' => 'multipack',
+                'label' => 'Multipack',
+                'required' => false
             ],[
                 'key' => 'availability_date',
                 'label' => 'Availability Date',
+                'required' => false
+            ],[
+                'key' => 'cost_of_goods_sold',
+                'label' => 'Cost of Goods Sold',
+                'required' => false
+            ],[
+                'key' => 'expiration_date',
+                'label' => 'Expiration Date',
                 'required' => false
             ],[
                 'key' => 'price',
@@ -108,14 +122,48 @@ class GeneralDataProvider extends BaseDataProvider
                 'label' => 'Item Group Id',
                 'required' => false
             ],[
+                'key' => 'product_highlight',
+                'label' => 'Product Highlight',
+                'required' => false
+            ],[
+                'key' => 'ads_redirect',
+                'label' => 'Ads Redirect',
+                'required' => false
+            ],[
                 'key' => 'shipping',
                 'label' => 'Shipping',
+                'required' => false
+            ],[
+                'key' => 'shipping_label',
+                'label' => 'Shipping Label',
                 'required' => false
             ],[
                 'key' => 'shipping_weight',
                 'label' => 'Shipping Weight',
                 'required' => false
             ],[
+                'key' => 'ships_from_country',
+                'label' => 'Ships From Country',
+                'required' => false
+            ],[
+                'key' => 'transit_time_label',
+                'label' => 'Transit Time Label',
+                'required' => false
+            ],[
+                'key' => 'max_handling_time',
+                'label' => 'Max Handling Time',
+                'required' => false
+            ],[
+                'key' => 'min_handling_time',
+                'label' => 'Min Handling Time',
+                'required' => false
+            ],[
+                'key' => 'tax_category',
+                'label' => 'Tax Category',
+                'required' => false
+            ],
+            //TODO Do we still need it ?
+            [
                 'key' => 'adwords_redirect',
                 'label' => 'Adwords Redirect',
                 'required' => false
@@ -142,6 +190,14 @@ class GeneralDataProvider extends BaseDataProvider
             ],[
                 'key' => 'custom_label_4',
                 'label' => 'Custom label 4',
+                'required' => false
+            ],[
+                'key' => 'promotion_id',
+                'label' => 'Promotion Id',
+                'required' => false
+            ],[
+                'key' => 'shopping_ads_excluded_country',
+                'label' => 'Shopping Ads Excluded Country',
                 'required' => false
             ]
         ];
